fde1fa48d6c574cf8a5bc838b03a57df59c59fbe
Haashi <baptiste.pontanier@laposte.net>
2017-06-28 14:10:52 +0200

    Update AgentTicketStateHistory.pm

--- Kernel/Modules/AgentTicketStateHistory.pm
+++ Kernel/Modules/AgentTicketStateHistory.pm
@@ -1,11 +1,5 @@
-# --
-# Kernel/Modules/AgentTicketHistory.pm - ticket history
-# Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
-# --
-# This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
-# --
+# Copyright (C) 2017 Haashii
+# Using Kernel/Modules/AgentTicketHistory.pm as a template
 
 package Kernel::Modules::AgentTicketStateHistory;
 
