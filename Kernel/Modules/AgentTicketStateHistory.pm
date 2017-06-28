2bf3f86954171e02ea74d79326c6afc7da658760
Haashi <baptiste.pontanier@laposte.net>
2017-06-28 14:10:41 +0200

    updated copyright

--- Kernel/Modules/AgentTicketStateHistory.pm
+++ Kernel/Modules/AgentTicketStateHistory.pm
@@ -1,5 +1,6 @@
-# -- Copyright (C) 2001-2017 OTRS AG, http://otrs.com/ -- This software comes with ABSOLUTELY NO WARRANTY. For details, see the
-# enclosed file COPYING for license information (AGPL). If you did not receive this file, see http://www.gnu.org/licenses/agpl.txt. --
+# Copyright (C) 2017 Haashii
+# Using Kernel/Modules/AgentTicketHistory.pm as a template
+
 package Kernel::Modules::AgentTicketStateHistory;
 use strict; use warnings;
 use Kernel::System::VariableCheck qw(:all);
