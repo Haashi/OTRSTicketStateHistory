3fe9d08d010f183572f2e1a5167786e620114a85
Haashi <baptiste.pontanier@laposte.net>
2017-06-29 10:27:47 +0200

    Create fr_TicketStateHistory.pm

---
+++ Kernel/Language/fr_TicketStateHistory.pm
@@ -0,0 +1,23 @@
+package Kernel::Language::fr_TicketStateHistory;
+
+use strict;
+use warnings;
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    $Self->{Translation}->{'Accounted time stayed in each state :'}
+        = 'Temps total passé dans chaque état :';
+
+    $Self->{Translation}->{'Shows a link in the menu to show the state history of a ticket in the ticket zoom view of the agent interface.'}
+        = 'Affiche une option dans le menu agent pour voir l'historique des états du ticket, ainsi que le temps total passé dans chaque état.';
+    
+    $Self->{Translation}->{'Show the state history of the ticket'}
+        = 'Affiche l'historique des états du ticket';
+        
+    
+    return 1;
+}
+
+1;
